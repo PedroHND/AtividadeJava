6c358b51a548e971ac727844a8ca1bdd1ff64abd
Pedro <nunespedro03@gmail.com>
2022-02-03 10:12:34 -0300

    Crição da classe quiz para os testes

---
+++ seis/Quiz.java
@@ -0,0 +1,22 @@
+package questao.seis;
+
+import java.util.Scanner;
+
+public class Quiz {
+	public String nome;
+	
+	Scanner scan = new Scanner(System.in);
+	
+	Questao q1 = new Questao("Quem venceu o último Super Bowl?", "Buccaneers", "Patriots", "Chiefs", "49ers", 1);
+	Questao q2 = new Questao("Quem foi o MVP da última temporada?", "Tom Brady", "Patrick Mahomes", "Cam Newton", "Aaron Rodgers", 4);
+	Questao q3 = new Questao("Qual desses jogadores não se aposentou ainda?", "Peyton Manning", "Andrew Luck", "Matt Ryan", "Tom Brady", 3);
+	Questao q4 = new Questao("Quem possui mais titulos na era Super Bowl", "Patriots", "Peyton Manning", "Steelers", "Tom Brady", 4);
+	
+	System.out.println("Digite o seu nome");
+	nomeParticipante = scan.next();
+	
+	Participante p = new Participante(nomeParticipante);
+	
+	
+}
+
