d94218fb814ee2fab159c9ec4bf2869183ce987a
Pedro <nunespedro03@gmail.com>
2022-02-05 09:02:47 -0300

    Criacao da classe main e adicao de seus comentarios

---
+++ sete/Main.java
@@ -0,0 +1,18 @@
+/*Classe Main, criada no dia 05/02/2022
+ * 
+ * Todos os direitos reservados a Compasso.UOL e Pedro Henrique Nunes Dutra
+ * 
+ * Classe que será utilizada para realizar o login dos usuários e retornar uma mensagem de acordo com o resultado e a hora.
+ * 
+ * */
+
+package questao.sete;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
