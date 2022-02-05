757a0577f43f0334dce37e22d6e5393fa511605b
Pedro <nunespedro03@gmail.com>
2022-02-05 08:55:57 -0300

    adição do construtor na classe Usuario

--- sete/Usuario.java
+++ sete/Usuario.java
@@ -11,6 +11,12 @@ package questao.sete;
 public class Usuario {
 	private String nome;
 	private String senha;
+	
+	public Usuario(String nome, String senha) {
+		this.nome = nome;
+		this.senha = senha;
+	}
+	
 	public String getNome() {
 		return nome;
 	}
