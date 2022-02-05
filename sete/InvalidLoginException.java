cb9016a61ddfd1e37c5d84a46208dee2d9dbbaf5
Pedro <nunespedro03@gmail.com>
2022-02-05 18:34:12 -0300

    adição de comentários na classe InvalidLoginException

--- sete/InvalidLoginException.java
+++ sete/InvalidLoginException.java
@@ -1,3 +1,11 @@
+/*Classe InvalidLoginException, criada no dia 05/02/2022
+ * 
+ * Todos os direitos reservados a Compasso.UOL e Pedro Henrique Nunes Dutra
+ * 
+ * Classe que será utilizada para criar uma exceção em caso de erros.
+ * 
+ * */
+
 package questao.sete;
 
 public class InvalidLoginException extends RuntimeException{
