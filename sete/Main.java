0f8c45bd5121f3098eed65d85742b01b74b55b95
Pedro <nunespedro03@gmail.com>
2022-02-05 18:32:36 -0300

    adição do if que verifica o login e correção do tipo de variável que armazena as horas

--- sete/Main.java
+++ sete/Main.java
@@ -8,9 +8,30 @@
 
 package questao.sete;
 
+import java.util.Calendar;
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
+		
+		Usuario user = new Usuario("Pedro", "2022");
+		String nome, senha;
+		
+		
+		Calendar cal =  Calendar.getInstance();
+		Scanner scan =  new Scanner(System.in);
+		int hora = cal.get(Calendar.HOUR_OF_DAY);
+		
+		System.out.println("Digite o seu usuário:");
+		nome = scan.next();
+		System.out.println("Digite a sua senha:");
+		senha = scan.next();
+		
+		if ((user.getSenha() != senha) || (user.getNome() != nome)) {
+			throw new InvalidLoginException("Usuário e/ou senha incorretos");
+		}
+		
 		
 
 	}
