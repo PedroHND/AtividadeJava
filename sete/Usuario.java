0e7c55149cf6ff6da91313b1c2368f65acc3fca3
Pedro <nunespedro03@gmail.com>
2022-02-05 08:52:01 -0300

    Criação da classe usuario com getters and setters e comentários.

---
+++ sete/Usuario.java
@@ -0,0 +1,28 @@
+/*Classe Participante, criada no dia 03/02/2022
+ * 
+ * Todos os direitos reservados a Compasso.UOL e Pedro Henrique Nunes Dutra
+ * 
+ * Classe que será utilizada na criação de um usuário e será logada na classe main.
+ * 
+ * */
+
+package questao.sete;
+
+public class Usuario {
+	private String nome;
+	private String senha;
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getSenha() {
+		return senha;
+	}
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	
+	
+}
