e0d67f0d5d87b4ca12696e0a86b5356c474245fd
Pedro <nunespedro03@gmail.com>
2022-02-05 19:03:04 -0300

    Finalização da classe main do package sete

--- sete/Main.java
+++ sete/Main.java
@@ -15,20 +15,36 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		Usuario user = new Usuario("Pedro", "2022");
+		Usuario user = new Usuario("Pedro", "JuB2022");
 		String nome, senha;
 		
 		
 		Calendar cal =  Calendar.getInstance();
 		Scanner scan =  new Scanner(System.in);
 		int hora = cal.get(Calendar.HOUR_OF_DAY);
+		String senhaUser = user.getSenha();
 		
 		System.out.println("Digite o seu usuário:");
 		nome = scan.next();
 		System.out.println("Digite a sua senha:");
 		senha = scan.next();
 		
-		if ((user.getSenha() != senha) || (user.getNome() != nome)) {
+		//função que verifica se o login do usuário está correto 
+		if (user.getNome().equals(nome) && user.getSenha().equals(senha)) {
+			if (hora >= 6 && hora<12) {
+				System.out.println("Bom dia, você se logou ao nosso sistema.");
+			}
+			else if(hora >= 12 && hora <18) {
+				System.out.println("Boa tarde, você se logou ao nosso sistema.");
+			}
+			else if(hora >= 18 && hora <=23) {
+				System.out.println("Boa noite, você se logou ao nosso sistema.");
+			}
+			else {
+				System.out.println("Boa madrugada, você se logou ao nosso sistema.");
+			}
+		}
+		else {
 			throw new InvalidLoginException("Usuário e/ou senha incorretos");
 		}
 		
